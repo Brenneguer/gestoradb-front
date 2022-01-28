cf5f5e1007019bbea9c45ac6e396c1001c4e77d6
Brenneguer <wbrenneguer07@gmail.com>
2022-01-28 00:24:50 -0300

    Refatorando PlayersRestService.ts

--- src/services/playerRestService.ts
+++ src/services/playerRestService.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { Unity } from './unityRestService';
+
 const url = process.env.REACT_APP_SERVER_URL;
 
 export type Player = {
@@ -10,59 +11,46 @@ export type Player = {
     unity: Unity;
 }
 
-async function findPlayerById(id: number) {
-    if (!playerIdValido(id)) return;
-    const user = await axios({
-        method: 'GET',
-        url: `${url}/players/${id}`
-    }).then((response) => response.data);
-    return user;
+async function findPlayerById(id: number) : Promise<Player> {
+  return axios({
+    method: 'GET',
+    url: `${url}/players/${id}`,
+  }).then((response) => response.data);
 }
 
-async function findAllPlayersByIdUnity(idUnity: number) {
-    const players = axios({
-        method: 'GET',
-        url: `${url}/players`,
-        params: { unity: idUnity }
-    }).then((response) => response.data)
-        .catch((error) => console.log(error.data))
-    return players;
+async function findAllPlayersByIdUnity(idUnity: number) : Promise<Player> {
+  return axios({
+    method: 'GET',
+    url: `${url}/players`,
+    params: { unity: idUnity },
+  }).then((response) => response.data)
+    .catch((error) => console.log(error.data));
 }
 
-async function createPlayer(player: Player) {
-    const response = axios({
-        method: 'POST',
-        url: `${url}/players`,
-        data: player,
-        params: { 'unity': player.unity.id }
-    }).then((response) => response.data);
-    return response;
+async function createPlayer(player: Player) : Promise<Player> {
+  return axios({
+    method: 'POST',
+    url: `${url}/players`,
+    data: player,
+    params: { unity: player.unity.id },
+  }).then((response) => response.data);
 }
 
-async function updatePlayer(player: Player) {
-    if (!playerIdValido(player.id)) return;
-    const response = axios({
-        method: 'PUT',
-        url: `${url}/players/${player.id}`,
-        data: player,
-    }).then((response) => response.data);
-    return response;
+async function updatePlayer(player: Player) : Promise<Player> {
+  return axios({
+    method: 'PUT',
+    url: `${url}/players/${player.id}`,
+    data: player,
+  }).then((response) => response.data);
 }
 
 async function deletePlayer(player: Player) {
-    if (!playerIdValido(player.id)) return;
-    const response = axios({
-        method: 'DELETE',
-        url: `${url}/players/${player.id}`
-    })
-    return response;
+  await axios({
+    method: 'DELETE',
+    url: `${url}/players/${player.id}`,
+  });
 }
 
-const playerIdValido = (id: any) => {
-    if (id === undefined || id === 0) {
-        return false;
-    }
-    return true;
-}
-
-export { findPlayerById, findAllPlayersByIdUnity };
+export {
+  findPlayerById, findAllPlayersByIdUnity, createPlayer, updatePlayer, deletePlayer,
+};
