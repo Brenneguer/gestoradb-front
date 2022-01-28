3c5c716403395e74a468b9859f557c82480008d6
Brenneguer <wbrenneguer07@gmail.com>
2022-01-28 00:25:24 -0300

    Refatorando UnityRestService.ts

--- src/services/unityRestService.ts
+++ src/services/unityRestService.ts
@@ -1,58 +1,50 @@
 import axios from 'axios';
 
-const url = process.env.REACT_APP_SERVER_URL
+const url = process.env.REACT_APP_SERVER_URL;
 export type Unity = {
     id?: string | number | undefined;
     nome: string;
     description: string;
 }
 
-async function findUnityById(id: string | number ) {
-    if (!unityIdValido(id)) return;
-    const response = axios({
-        method: 'GET',
-        url: `${url}/unity/${id}`
-    });
+async function findUnityById(id: string | number) : Promise<Unity> {
+  return axios({
+    method: 'GET',
+    url: `${url}/unity/${id}`,
+  }).then((response) => response.data);
 }
 
 async function findAllUnities() {
-    return axios({
-        method: 'GET',
-        url: `${url}/unities`
-    }).then((response) => response.data);
+  return axios({
+    method: 'GET',
+    url: `${url}/unities`,
+  }).then((response) => response.data);
 }
 
-async function createUnity(unity: Unity) {
-    const response = axios({
-        method: 'POST',
-        url: `${url}/unities`,
-        data: unity
-    }).then((response) => response.data);
+async function createUnity(unity: Unity) : Promise<Unity> {
+  return axios({
+    method: 'POST',
+    url: `${url}/unities`,
+    data: unity,
+  }).then((response) => response.data);
 }
 
-async function updateUnity(unity: Unity) {
-    if (!unityIdValido(unity.id)) return;
-    const response = axios({
-        method: 'PUT',
-        url: `${url}/unities/${unity.id}`,
-        data: unity
-    }).then((response) => response.data);
+async function updateUnity(unity: Unity) : Promise<Unity> {
+  return axios({
+    method: 'PUT',
+    url: `${url}/unities/${unity.id}`,
+    data: unity,
+  }).then((response) => response.data);
 }
 
 async function deleteUnity(unity: Unity) {
-    if (!unityIdValido(unity.id)) return;
-    const response = axios({
-        method: 'DELETE',
-        url: `${url}/unities/${unity.id}`,
-        data: unity
-    }).then((response) => response.data);
+  axios({
+    method: 'DELETE',
+    url: `${url}/unities/${unity.id}`,
+    data: unity,
+  }).then((response) => response.data);
 }
 
-const unityIdValido = (id: any) => {
-    if (id === undefined || id === 0) {
-        return false;
-    }
-    return true;
-}
-
-export { findUnityById, findAllUnities }
+export {
+  findUnityById, findAllUnities, createUnity, updateUnity, deleteUnity,
+};
