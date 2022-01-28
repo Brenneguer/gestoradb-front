abd9db6dc7891673c0c3519cb8f2f73139eed313
Brenneguer <wbrenneguer07@gmail.com>
2022-01-28 11:07:36 -0300

    controle de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { Home } from './pages/Home';
+import { Unities } from './pages/Unities';
+import { Players } from './pages/Players';
 
 import './styles/global.css';
 
@@ -10,6 +12,8 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/unities" exact component={Unities} />
+        <Route path="/players" exact component={Players} />
       </Switch>
     </BrowserRouter>
   );
