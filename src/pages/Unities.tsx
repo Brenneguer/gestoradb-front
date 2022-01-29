3b2e9047e3429aba5acb578592e17186511c1e5f
Brenneguer <wbrenneguer07@gmail.com>
2022-01-29 12:18:55 -0300

    Adicionando tabela de players a unity

--- src/pages/Unities.tsx
+++ src/pages/Unities.tsx
@@ -1,43 +1,68 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
+import {
+  Button,
+  Container,
+  FormControl,
+  InputGroup,
+} from 'react-bootstrap';
 import { Unity } from '../services/unityRestService';
 import { useAdb } from '../hooks/useAdb';
 import { NavBar } from '../components/NavBar';
+
+import '../styles/unity.css';
+import { Players } from './Players';
 
 export function Unities() {
   const history = useHistory();
   const [unity, setUnity] = useState<Unity>();
   const { unityContext } = useAdb();
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleUpdateName = (newName: string) => setName(newName);
+  const handleUpdateDescription = (newDescription: string) => setDescription(newDescription);
 
   useEffect(() => {
-    if (unityContext === undefined) {
+    if (unityContext === undefined || unityContext.name === undefined) {
       history.push('/');
+      return;
     }
     setUnity(unity);
+    setName(unityContext.name);
+    setDescription(unityContext.description);
   }, [unityContext, unity, history]);
 
   return (
     <>
       <NavBar title="unities" />
-      <Container className="tableUnities">
-        <div>
+      <Container>
+        <div className="unities">
           <div>
-            {
-              unityContext?.name
-            }
+            <InputGroup className="mb-3">
+              <InputGroup.Text>Name</InputGroup.Text>
+              <FormControl
+                value={name}
+                type="input"
+                onChange={(event) => handleUpdateName(event.target.value)}
+              />
+            </InputGroup>
           </div>
           <div>
-            {
-              unityContext?.description
-            }
+            <InputGroup className="mb-3">
+              <InputGroup.Text>Description</InputGroup.Text>
+              <FormControl
+                value={description}
+                as="textarea"
+                onChange={(event) => handleUpdateDescription(event.target.value)}
+              />
+            </InputGroup>
           </div>
-          <div>
-            {
-              unityContext?.id
-            }
-          </div>
+          <Button>Save</Button>
         </div>
+      </Container>
+      <Container>
+        <Players unity={unityContext} />
       </Container>
 
     </>
