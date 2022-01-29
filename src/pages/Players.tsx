0ea47ec56623a30104607feeaf2b7dff4b8fc443
Brenneguer <wbrenneguer07@gmail.com>
2022-01-29 12:17:42 -0300

    Atualizando player, adicionando props unity

--- src/pages/Players.tsx
+++ src/pages/Players.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
+import { findAllPlayersByIdUnity, Player } from '../services/playerRestService';
+import { Unity } from '../services/unityRestService';
 
-export function Players() {
+export type PlayersProps = {
+  unity: Unity | undefined;
+}
+
+export function Players(props: PlayersProps) {
+  const { unity } = props;
+  const [players, setPlayers] = useState<Player[]>([]);
+
+  useEffect(() => {
+    async function findPlayers() {
+      const result = await findAllPlayersByIdUnity(unity?.id);
+      setPlayers(result);
+    }
+    if (players.length === 0) {
+      findPlayers().then();
+    }
+  });
+
+  const dateFormat = (date: string):string => `${date.slice(8, 10)}/${date.slice(5, 7)}/${date.slice(0, 4)}`;
+
   return (
     <div>
-      <h1>Eu vivi para ver</h1>
+      <Table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Birth</th>
+            <th>Bio</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            players?.map(
+              (player) => (
+                <tr key={player.id}>
+                  <td>{player.nick}</td>
+                  <td>{dateFormat(player.birth)}</td>
+                  <td>{player.bio}</td>
+                  <td>Open</td>
+                </tr>
+              ),
+            )
+          }
+        </tbody>
+      </Table>
     </div>
   );
 }
